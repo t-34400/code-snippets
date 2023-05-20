46aa4d2c2298108eac7844f5ace1fe048a47f8ba
Takumi Miyoshi <34400mt@gmail.com>
2023-05-21 04:39:00 +0900

    Add wavedoc.py

---
+++ Python/PyWavelets/wavedoc.py
@@ -0,0 +1,27 @@
+import pywt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = np.linspace(0, 1, 1000)
+data = np.sin(50 * 2 * np.pi * t) * np.exp(-10 * t)
+
+wavelet = 'db4'
+level = 5
+
+coeffs = pywt.wavedec(data, wavelet, level=level)
+
+
+plt.figure(figsize=(10, 6))
+plt.subplot(level + 2, 1, 1)
+reconstructed_data = pywt.waverec(coeffs, wavelet)
+plt.plot(data)
+plt.plot(reconstructed_data)
+
+for i, c in enumerate(coeffs):
+    plt.subplot(level + 2, 1, i + 2)
+    plt.plot(c)
+    plt.ylabel(f"Level {i+1}")
+plt.xlabel("Sample")
+plt.tight_layout()
+plt.show()
