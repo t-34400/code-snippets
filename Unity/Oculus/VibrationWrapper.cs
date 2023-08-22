4fd5c6c2ec4a531876ba258d76e36309a798853f
t-34400 <49368264+t-34400@users.noreply.github.com>
2023-08-22 16:26:58 +0900

    Update VibrationWrapper.cs

--- Unity/Oculus/VibrationWrapper.cs
+++ Unity/Oculus/VibrationWrapper.cs
@@ -1,139 +1,148 @@
 #nullable enable
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+
 using UnityEngine;
 
-internal class VibrationSettings
+namespace OculusHelpers
 {
-    private float _frequency;
-    private float _amplitude;
+    record VibrationSettings(float frequency = 1.0f, float amplitude = 1.0f);
 
-    internal VibrationSettings(float frequency = 1.0f, float amplitude = 1.0f)
+    class VibrationWrapper : MonoBehaviour
     {
-        this._frequency = frequency;
-        this._amplitude = amplitude;
-    }
+        // Controller vibration automatically ends two seconds after the last input,
+        // so the next input must be entered periodically.
+        private const float INPUT_INTERVAL_SEC = 1.0f;
 
-    internal float frequency => _frequency;
-    internal float amplitude => _amplitude;
-}
+        private readonly List<(Guid vibrationId, VibrationSettings settings)> leftHandVibrationSettingsList = new();
+        private readonly List<(Guid vibrationId, VibrationSettings settings)> rightHandVibrationSettingsList = new();
 
-class VibrationWrapper : MonoBehaviour
-{
-    // Controller vibration automatically ends two seconds after the last input,
-    // so the next input must be entered periodically.
-    private const float INPUT_INTERVAL_SEC = 1.0f;
+        private bool isLeftHandCoroutineRunning = false;
+        private bool isRightHandCoroutineRunning = false;
 
-    private readonly List<VibrationSettings> leftHandVibrationSettingsList = new List<VibrationSettings>();
-    private readonly List<VibrationSettings> rightHandVibrationSettingsList = new List<VibrationSettings>();
-
-    private bool isLeftHandCoroutineRunning = false;
-    private bool isRightHandCoroutineRunning = false;
-
-    public void StartLeftHandVibration(VibrationSettings vibrationSettings, float? duration = null)
-    {
-        leftHandVibrationSettingsList.Add(vibrationSettings);
-
-        if(!isLeftHandCoroutineRunning)
+        public Guid StartLeftHandVibration(VibrationSettings vibrationSettings, float? duration = null)
         {
-            isLeftHandCoroutineRunning = true;
-            StartCoroutine(Vibrate(OVRInput.Controller.LHand));
-        }
-    }
+            var vibrationId = new Guid();
+            leftHandVibrationSettingsList.Add((vibrationId, vibrationSettings));
 
-    public void StartRightHandVibration(VibrationSettings vibrationSettings, float? duration = null)
-    {
-        rightHandVibrationSettingsList.Add(vibrationSettings);
-
-        if(!isRightHandCoroutineRunning)
-        {
-            isRightHandCoroutineRunning = true;
-            StartCoroutine(Vibrate(OVRInput.Controller.RHand));
-        }
-    }
-
-    public void EndLeftHandVibration(VibrationSettings vibrationSettings)
-    {
-        leftHandVibrationSettingsList.Remove(vibrationSettings);
-    }
-
-    public void EndRightHandVibration(VibrationSettings vibrationSettings)
-    {
-        rightHandVibrationSettingsList.Remove(vibrationSettings);
-    }
-
-    private IEnumerator Vibrate(OVRInput.Controller controller)
-    {
-        var vibrationSettingsList = GetVibrationSettingsList(controller);
-        if(vibrationSettingsList == null)
-        {
-            yield break;
-        }
-
-        var previousInputTime = 0.0f;
-        VibrationSettings? previousVibrationSettings = null;
-        
-        while(vibrationSettingsList.Count > 0)
-        {
-            var currentTime = Time.time;
-            var currentVibrationSettings = vibrationSettingsList.Last();
-            
-            if(currentVibrationSettings == previousVibrationSettings && currentTime - previousInputTime < INPUT_INTERVAL_SEC)
+            if(!isLeftHandCoroutineRunning)
             {
+                isLeftHandCoroutineRunning = true;
+                StartCoroutine(Vibrate(OVRInput.Controller.LTouch));
+            }
+
+            if(duration != null && duration > 0.0f)
+            {
+                StartCoroutine(StopVibrationAfterDelay(OVRInput.Controller.LTouch, vibrationId, duration!.Value));
+            }
+
+            return vibrationId;
+        }
+
+        public Guid StartRightHandVibration(VibrationSettings vibrationSettings, float? duration = null)
+        {
+            var vibrationId = new Guid();
+            rightHandVibrationSettingsList.Add((vibrationId, vibrationSettings));
+
+            if(!isRightHandCoroutineRunning)
+            {
+                isRightHandCoroutineRunning = true;
+                StartCoroutine(Vibrate(OVRInput.Controller.RTouch));
+            }
+
+            if(duration != null && duration > 0.0f)
+            {
+                StartCoroutine(StopVibrationAfterDelay(OVRInput.Controller.RTouch, vibrationId, duration!.Value));
+            }
+
+            return vibrationId;
+        }
+
+        public void StopLeftHandVibration(Guid vibrationId)
+        {
+            leftHandVibrationSettingsList.RemoveAll(pair => pair.vibrationId == vibrationId);
+        }
+
+        public void StopRightHandVibration(Guid vibrationId)
+        {
+            rightHandVibrationSettingsList.RemoveAll(pair => pair.vibrationId == vibrationId);
+        }
+
+        private IEnumerator Vibrate(OVRInput.Controller controller)
+        {
+            var vibrationSettingsList = GetVibrationSettingsList(controller);
+            if(vibrationSettingsList == null)
+            {
+                yield break;
+            }
+
+            var previousInputTime = 0.0f;
+            Guid? previousVibrationId = null;
+            
+            while(vibrationSettingsList.Count > 0)
+            {
+                var currentTime = Time.time;
+                var currentPair = vibrationSettingsList.Last();
+                
+                if(currentPair.vibrationId == previousVibrationId && currentTime - previousInputTime < INPUT_INTERVAL_SEC)
+                {
+                    yield return null;
+                }
+
+                previousInputTime = currentTime;
+                previousVibrationId = currentPair.vibrationId;
+                OVRInput.SetControllerVibration(currentPair.settings.frequency, currentPair.settings.amplitude, controller);
                 yield return null;
             }
 
-            previousInputTime = currentTime;
-            previousVibrationSettings = currentVibrationSettings;
-            OVRInput.SetControllerVibration(currentVibrationSettings.frequency, currentVibrationSettings.amplitude, controller);
-            yield return null;
+            OVRInput.SetControllerVibration(0.0f, 0.0f, controller);
+            SetCoroutineRunningStateFalse(controller);
         }
 
-        OVRInput.SetControllerVibration(0.0f, 0.0f, controller);
-        SetCoroutineRunningStateFalse(controller);
-    }
-
-    private List<VibrationSettings>? GetVibrationSettingsList(OVRInput.Controller controller)
-    {
-        switch(controller)
+        private IEnumerator StopVibrationAfterDelay(OVRInput.Controller controller, Guid vibrationId, float delay)
         {
-            case OVRInput.Controller.LTouch:
-                {
-                    return leftHandVibrationSettingsList;
-                }
-            case OVRInput.Controller.RTouch:
-                {
-                    return rightHandVibrationSettingsList;
-                }
-            default:
-                {
-                    return null;
-                }
+            yield return new WaitForSeconds(delay);
+
+            GetVibrationSettingsList(controller)?.RemoveAll(pair => pair.vibrationId == vibrationId);;
         }
-    }
 
-    private IEnumerator StopVibrationAfterDelay(OVRInput.Controller controller, VibrationSettings vibrationSettings, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        GetVibrationSettingsList(controller)?.Remove(vibrationSettings);
-    }
-
-    private void SetCoroutineRunningStateFalse(OVRInput.Controller controller)
-    {
-        switch(controller)
+        private List<(Guid vibrationId, VibrationSettings settings)>? GetVibrationSettingsList(OVRInput.Controller controller)
         {
-            case OVRInput.Controller.LTouch:
-                {
-                    isLeftHandCoroutineRunning = false;
-                    return;
-                }
-            case OVRInput.Controller.RTouch:
-                {
-                    isRightHandCoroutineRunning = false;
-                    return;
-                }
+            switch(controller)
+            {
+                case OVRInput.Controller.LTouch:
+                    {
+                        return leftHandVibrationSettingsList;
+                    }
+                case OVRInput.Controller.RTouch:
+                    {
+                        return rightHandVibrationSettingsList;
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        private void SetCoroutineRunningStateFalse(OVRInput.Controller controller)
+        {
+            switch(controller)
+            {
+                case OVRInput.Controller.LTouch:
+                    {
+                        isLeftHandCoroutineRunning = false;
+                        return;
+                    }
+                case OVRInput.Controller.RTouch:
+                    {
+                        isRightHandCoroutineRunning = false;
+                        return;
+                    }
+            }
         }
     }
 }
