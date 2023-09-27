364a77c18c75c367ec67688787f321d4530c6b03
t-34400 <49368264+t-34400@users.noreply.github.com>
2023-09-27 17:12:18 +0900

    Update BoxAlignmentHelper.cs

--- Unity/utilities/BoxAlignmentHelper.cs
+++ Unity/utilities/BoxAlignmentHelper.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public static class BoxAlignmentHelper
 {
-    public static Quaternion GetTargetWorldRotation()
+    public static Quaternion GetTargetWorldRotation(Quaternion objectRotation)
     {
-        var handleRotation = boxObject.rotation;
+        var localRightVector = objectRotation * Vector3.right;
+        var localUpVector = objectRotation * Vector3.up;
+        var localForwardVector = objectRotation * Vector3.forward;
 
-        var rightVector = boxObject.right;
-        var upVector = boxObject.up;
-        var forwardVector = boxObject.forward;
-
-        var downwardFaceVector = GetDownwardFaceVector(rightVector, upVector, forwardVector);
-        return Quaternion.FromToRotation(downwardFaceVector, Vector3.down) * handleRotation;
+        var downwardFaceVector = GetDownwardFaceVector(localRightVector, localUpVector, localForwardVector);
+        return Quaternion.FromToRotation(downwardFaceVector, Vector3.down) * objectRotation;
     }
 
     private static Vector3 GetDownwardFaceVector(Vector3 rightVector, Vector3 upVector, Vector3 forwardVector)
@@ -37,6 +35,30 @@ public static class BoxAlignmentHelper
         else
         {
             return (forwardComponent > 0) ? forwardVector : -forwardVector;
+        }
+    }
+
+    private static (Vector3 down, Vector3 width, Vector3 depth) GetDownWidthDepthVectors(Vector3 localRightVector, Vector3 localUpVector, Vector3 localForwardVector)
+    {
+        var rightComponent = Vector3.Dot(Vector3.down, localRightVector);
+        var upComponent = Vector3.Dot(Vector3.down, localUpVector);
+        var forwardComponent = Vector3.Dot(Vector3.down, localForwardVector);
+
+        var absRightComponent = Mathf.Abs(rightComponent);
+        var absUpComponent = Mathf.Abs(upComponent);
+        var absForwardComponent = Mathf.Abs(forwardComponent);
+
+        if(absUpComponent >= absRightComponent && absUpComponent >= absForwardComponent)
+        {
+            return (upComponent > 0) ? (localUpVector, -localRightVector, localForwardVector) : (-localUpVector, localRightVector, -localForwardVector);
+        }
+        else if(absRightComponent >= absUpComponent && absRightComponent >= absForwardComponent)
+        {
+            return (rightComponent > 0) ? (localRightVector, localForwardVector, -localUpVector) : (-localRightVector, -localForwardVector, localUpVector);
+        }
+        else
+        {
+            return (forwardComponent > 0) ? (localForwardVector, localUpVector, -localRightVector) : (-localForwardVector, -localUpVector, localRightVector);
         }
     }
 }
