62468c0eec1d83467ed435257c2ae0533ace3902
Takumi Miyoshi <34400mt@gmail.com>
2023-12-31 21:57:28 +0900

    Refactor to fix dependencies in the input controller tab feature

--- Python/VRChat_OSC_Sender/views/input_controller_tab.py
+++ Python/VRChat_OSC_Sender/views/input_controller_tab.py
@@ -6,7 +6,7 @@ from services.input_controller_tab_services import InputControllerTabServices
 class InputControllerTab:
     def __init__(self, root, sender):
         self.input_controller_params = InputControllerParams()
-        self.services = InputControllerTabServices(sender)
+        self.services = InputControllerTabServices(sender, self.input_controller_params)
 
         self.axes_group = ttk.LabelFrame(root, text="Axes")
         self.axes_group.pack(padx=10, pady=10, fill="both", expand=True, anchor=tk.W)
@@ -14,10 +14,9 @@ class InputControllerTab:
         for index, axis in enumerate(self.input_controller_params.axes):
             label = tk.Label(self.axes_group, text=axis, anchor=tk.W)
             label.grid(row=index, column=0, sticky="e", padx=5, pady=5)
-
-            variable = self.input_controller_params.axis_vars[axis]
-            variable.trace_add("write", lambda *args, axis=axis, variable=variable: self.on_axis_value_change(axis, variable))
             
+            variable = self.input_controller_params.axis_vars[axis]
+
             scale = tk.Scale(self.axes_group, from_=-1.0, to=1.0, variable=variable, orient="horizontal", length=200, resolution=0.1)
             scale.grid(row=index, column=1, sticky="e", padx=5, pady=0)
 
@@ -35,18 +34,6 @@ class InputControllerTab:
             label.grid(row=row, column=column, sticky="e", padx=5, pady=5)
 
             variable = self.input_controller_params.button_vars[button]
-            variable.trace_add("write", lambda *args, button=button, variable=variable: self.on_button_value_change(button, variable))
-            
+
             scale = tk.Checkbutton(self.buttons_group, variable=variable)
             scale.grid(row=row, column=column+1, sticky="e", padx=5, pady=0)
-
-    
-    def on_axis_value_change(self, axis, variable):
-        self.services.send_axis_value(axis, variable.get())
-
-    def on_button_value_change(self, button, variable):
-        if variable.get():
-            value = 1
-        else:
-            value = 0
-        self.services.send_button_value(button, value)
