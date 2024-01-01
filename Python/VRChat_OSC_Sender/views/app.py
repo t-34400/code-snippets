2b8bb2c4fe18b2291eb8ed1a99bb7c3c0b342ae0
Takumi Miyoshi <34400mt@gmail.com>
2024-01-01 10:03:31 +0900

    Implement tracker tab feature

--- Python/VRChat_OSC_Sender/views/app.py
+++ Python/VRChat_OSC_Sender/views/app.py
@@ -2,6 +2,7 @@ from tkinter import ttk
 from .avatar_params_tab import AvatarParamsTab
 from .input_controller_tab import InputControllerTab
 from .chatbox_tab import ChatboxTab
+from .tracker_tab import TrackerTab
 from .osc_config_tab import OSCConfigTab
 
 class App:
@@ -22,6 +23,10 @@ class App:
         self.chatbox_tab_frame = ttk.Frame(self.notebook)
         ChatboxTab(self.chatbox_tab_frame, sender)
         self.notebook.add(self.chatbox_tab_frame, text="Chatbox")
+
+        self.tracker_tab_frame = ttk.Frame(self.notebook)
+        TrackerTab(self.tracker_tab_frame, sender)
+        self.notebook.add(self.tracker_tab_frame, text="Trackers")
 
         self.osc_config_frame = ttk.Frame(self.notebook)
         OSCConfigTab(self.osc_config_frame)
